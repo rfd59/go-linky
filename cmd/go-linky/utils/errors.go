17489d389c30fb2f2e90150ba3926f93d7ff668d
Richard FAVREAU <rfavreau@rfd.ovh>
2025-08-16 19:03:01 +0200

    Update

--- cmd/go-linky/utils/errors.go
+++ cmd/go-linky/utils/errors.go
@@ -4,8 +4,8 @@ import "errors"
 
 var ErrNoDatasets = errors.New("no datasets found in the frame")
 var ErrNoSerialPort = errors.New("no serial ports found")
-var ErrInvalidDataset = errors.New("invalid dataset format: %s")
-var ErrInvalidChecksum = errors.New("invalid checksum length: %s")
+var ErrInvalidDataset = errors.New("invalid dataset format")
+var ErrInvalidChecksum = errors.New("invalid checksum length")
 var ErrEmptyFrame = errors.New("frame is empty")
 var ErrSTXFrame = errors.New("frame does not start with STX (0x02) character")
 var ErrETXFrame = errors.New("frame does not end with ETX (0x03) character")
